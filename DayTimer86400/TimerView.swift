960cdc0b34262fe9c45b8dabb3f7c5bf6307bdaf
Funa4git <s1970223@ems.u-toyama.ac.jp>
2022-04-16 20:39:55 +0900

    1日の秒数の表示を実装

--- DayTimer86400/TimerView.swift
+++ DayTimer86400/TimerView.swift
@@ -9,37 +9,48 @@
 import SwiftUI
 
 struct TimerView: View {
+    @State var hour = Calendar.current.component(.hour, from: Date())
+    @State var minute = Calendar.current.component(.minute, from: Date())
+    @State var second = Calendar.current.component(.second, from: Date())
+    let daySecond = 24*60*60
+    let timer = Timer.publish(every: 1, on: .current, in: .common).autoconnect()
+    
     var body: some View {
         VStack {
             Spacer()
             VStack {
                 Text("current day time")
-                    .font(.largeTitle)
+                    .font(.title)
                 HStack {
-                    Text("50787")
+                    Text("\(hour*60*60 + minute*60 + second)")
                     Text("sec")
                 }
-                .font(.title)
+                .font(.largeTitle)
+            }
+            .onReceive(timer){ _ in
+                self.hour = Calendar.current.component(.hour, from: Date())
+                self.minute = Calendar.current.component(.minute, from: Date())
+                self.second = Calendar.current.component(.second, from: Date())
             }
             .padding()
             VStack {
                 Text("last day time")
-                    .font(.largeTitle)
+                    .font(.title)
                 HStack {
-                    Text("35613")
+                    Text("\(daySecond - (hour*60*60 + minute*60 + second))")
                     Text("sec")
                 }
-                    .font(.title)
+                .font(.largeTitle)
             }
             .padding()
             VStack {
                 Text("view of %")
-                    .font(.largeTitle)
+                    .font(.title)
                 HStack {
-                    Text("41.219")
+                    Text("\(100 * (hour*60*60 + minute*60 + second) / daySecond)")
                     Text("%")
                 }
-                    .font(.title)
+                .font(.largeTitle)
             }
             .padding()
             Spacer()
