78c65f651927ad7d45deb816ea02c4966a93cd19
Funa4git <s1970223@ems.u-toyama.ac.jp>
2022-04-16 17:15:41 +0900

    Initial Commit

---
+++ DayTimer86400/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  DayTimer86400
+//  
+//  Created by Funa on 2022/04/16
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DayTimer86400/DayTimer86400App.swift
@@ -0,0 +1,18 @@
+//
+//  DayTimer86400App.swift
+//  DayTimer86400
+//  
+//  Created by Funa on 2022/04/16
+//  
+//
+
+import SwiftUI
+
+@main
+struct DayTimer86400App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
