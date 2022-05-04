2e3f092e35ffd691d20021be50a11dcb3dd3e158
Funa4git <s1970223@ems.u-toyama.ac.jp>
2022-05-05 01:52:38 +0900

    watchOSに対応

--- WatchTimer86400 WatchKit Extension/ContentView.swift
+++ WatchTimer86400 WatchKit Extension/ContentView.swift
@@ -8,10 +8,56 @@
 
 import SwiftUI
 
+
+extension Color {
+    static let textColor = Color("TextColor")
+}
+
 struct ContentView: View {
+    @State private var nowDate = Date()
+    @State private var currentSecond = 0.0
+    @State private var lastSecond = 0.0
+    private let timer = Timer.publish(every: 0.1, on: .current, in: .common).autoconnect()
+    
+    func refreshCounter() {
+        nowDate = Date()
+        currentSecond = Double(nowDate.hour * 60 * 60 + nowDate.minute * 60 + nowDate.second)
+        lastSecond = 24 * 60 * 60 - currentSecond
+        
+    }
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            HStack {
+                Text("current day time")
+                    .fontWeight(.thin)
+                    .padding(.leading)
+                Spacer()
+            }
+            Text("\(currentSecond, specifier: "%.0f") sec")
+                .font(.title2)
+            HStack {
+                Text("last day time")
+                    .fontWeight(.thin)
+                    .padding(.leading)
+                Spacer()
+            }
+            Text("\(lastSecond, specifier: "%.0f") sec")
+                .font(.title2)
+            HStack {
+                Text("view of %")
+                    .fontWeight(.thin)
+                    .padding(.leading)
+                Spacer()
+            }
+            Text("\(lastSecond / 86400 * 100, specifier: "%.3f") %")
+                .font(.title2)
+        }
+        .foregroundColor(Color.textColor)
+        // 0.1秒に1回現在時刻を確認
+        .onReceive(timer){ _ in
+            refreshCounter()
+        }
     }
 }
 
